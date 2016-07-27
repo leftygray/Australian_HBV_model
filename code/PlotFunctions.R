8279cd0b4afb6159a86a381f45f6814aff35098a
leftygray <leftygray@gmail.com>
2016-07-28 02:26:16 +1000

    Set up plotting of non-population indicators

--- code/PlotFunctions.R
+++ code/PlotFunctions.R
@@ -6,6 +6,8 @@
 # the HBV model results and for figure generation. 
 
 
+# Extract annual results -------------------------------------------------
+
 MidyearIndex <- function(n, timestep) {
   # This function extracts the mid year index. This used for 
   # calculating per population number annual outputs by dividing
@@ -74,6 +76,7 @@ yearDf <- function(df, sumVar, years, npops, timestep,
   return(yearFrame)
 }
 
+# Organize results -------------------------------------------------------
 
 popResults <- function(pg, bestResults, paramResults,
                        populations = NULL, states = NULL,
@@ -116,7 +119,6 @@ popResults <- function(pg, bestResults, paramResults,
           summarise(total_pop = sum(popsize)) %>%
           spread(simulation, total_pop)
       } else if (is.null(states)) {
-        print("hi")
         popSizes <- popSizes %>%
           gather("simulation", "popsize", 4:ncol(popSizes)) %>%
           group_by(year, simulation, state) %>%
@@ -166,13 +168,12 @@ popResults <- function(pg, bestResults, paramResults,
   }
   
   # Ungroup in preparation for next manipulation 
-  
   popSizes <- ungroup(popSizes)
   bestValues <- popSizes$best
   
   if (range) {
     bestcol <- which(colnames(popSizes) == "best")
-
+    
     popSizes <- gather(popSizes, "sim", "popsize", 
                        bestcol:ncol(popSizes)) 
     
@@ -189,8 +190,16 @@ popResults <- function(pg, bestResults, paramResults,
     popSizes <- popSizes %>%
       summarise(min = min(popsize),
                 max = max(popsize)) %>%
-      ungroup() %>%
-      mutate(best = bestValues)
+      ungroup() 
+    
+    if (is.null(populations) && is.null(states)) {
+      popSizes <- popSizes %>%
+        arrange(population) %>%
+        mutate(best = bestValues)
+    } else {
+      popSizes <- popSizes %>%
+        mutate(best = bestValues)
+    }
     
     if (is.null(populations) && is.null(states)) {
       popSizes <- select(popSizes, year, population, state,
@@ -206,9 +215,148 @@ popResults <- function(pg, bestResults, paramResults,
   
   
   # Return the final population data frame
-  return(popSizes)
+  return(tbl_df(popSizes))
 }
 
+
+indicatorResults<- function(pg, bestResults, paramResults,
+                            indicator, populations = NULL, 
+                            range = FALSE, 
+                            annual = NULL) {
+  # This function organizes and merges the specific
+  # indicator results
+  
+  # annual = c("sum", "midyear", NULL)
+  
+  # First organize the best estimates
+  bestEstimates <- tbl_df(as.data.frame(t(bestResults[[indicator]]))) %>%
+    mutate(year = pg$pts) %>%
+    select(year, everything()) %>%
+    gather_("population", indicator, pg$population_names) %>%
+    rename_("best" = indicator) 
+  
+  # Now append each param set results
+  indicatorEstimates <- bestEstimates
+  
+  for (set in 1:pg$parameter_samples) {
+    paramSet <- paste0("paramSet", toString(set))
+    setIndicator <- 
+      tbl_df(as.data.frame(t(paramResults[[paramSet]][[indicator]]))) %>%
+      mutate(year = pg$pts) %>%
+      select(year, everything()) %>%
+      gather_("population", indicator, pg$population_names)
+    
+    # setIndicator <- arrange(setIndicator, population)
+    indicatorEstimates[, paste0("set", toString(set))] <-  
+      setIndicator[, indicator]
+  }
+  
+  # Convert population into a factor and order the levels
+  # indicatorEstimates$population <- factor(indicatorEstimates$population,
+  #                                         levels = c("age0to4", "age5to14",
+  #                                                    "age15to44", "age45"))
+  
+  # Extract the subresults we want
+  if (length(populations) == 1 && populations == "all") {
+    indicatorEstimates <- indicatorEstimates %>%
+      gather("simulation", "estimate", 3:ncol(indicatorEstimates)) %>%
+      group_by(year, simulation) %>%
+      summarise(total = sum(estimate)) %>%
+      spread(simulation, total)
+  } else if (is.null(populations)){
+    indicatorEstimates <- indicatorEstimates %>%
+      gather("simulation", "estimate", 3:ncol(indicatorEstimates)) %>%
+      group_by(year, simulation, population) %>%
+      summarise(total = sum(estimate)) %>%
+      spread(simulation, total)
+  } else {
+    indicatorEstimates <- indicatorEstimates %>%
+      filter(population %in% populations) %>%
+      gather("simulation", "estimate", 3:ncol(indicatorEstimates)) %>%
+      group_by(year, simulation) %>%
+      summarise(total = sum(estimate)) %>%
+      spread(simulation, total)
+  }  
+  
+  # Ungroup in preparation for next manipulation 
+  indicatorEstimates <- ungroup(indicatorEstimates)
+  if (is.null(populations)){
+    indicatorEstimates <- arrange(indicatorEstimates, population)
+  }
+  
+  # Extract best column location
+  bestcol <- which(colnames(indicatorEstimates) == "best")
+  
+  # Apply annual manipulations
+  if (!is.null(annual)) {
+    if (annual == "sum") {
+      tempEstimates <- indicatorEstimates[seq(1, nrow(indicatorEstimates), 
+                                              1 / pg$timestep), ]
+      tempEstimates[, bestcol:ncol(tempEstimates)] <- 
+        apply(indicatorEstimates[, bestcol:ncol(indicatorEstimates)],
+              2, function(x) SumYear(x, pg$timestep))
+      
+      if (is.null(populations)) {
+        tempEstimates$year <- rep(head(pg$years, -1), pg$npops)
+      } else {
+        tempEstimates$year <- head(pg$years, -1)
+      }
+      
+      indicatorEstimates <- tempEstimates
+      
+    } else if (annual == "midyear") {
+      tempEstimates <- indicatorEstimates[seq(1, nrow(indicatorEstimates), 
+                                              1 / pg$timestep), ]
+      tempEstimates <- 
+        indicatorEstimates[MidyearIndex(nrow(indicatorEstimates), 
+                                        pg$timestep), ]
+      if (is.null(populations)) {
+        tempEstimates$year <- rep(head(pg$years, -1), pg$npops)
+      } else {
+        tempEstimates$year <- head(pg$years, -1)
+      }
+      
+      indicatorEstimates <- tempEstimates
+      
+    }
+  }
+  
+  # Store best values for later
+  bestValues <- indicatorEstimates$best
+  
+  if (range) {
+   
+    indicatorEstimates <- gather(indicatorEstimates, "sim", "estimate", 
+                       bestcol:ncol(indicatorEstimates)) 
+    
+    if (is.null(populations)) {
+      indicatorEstimates <- indicatorEstimates %>%
+        group_by(year, population) %>%
+        summarise(min = min(estimate),
+                  max = max(estimate)) %>%
+        ungroup() %>%
+        arrange(population) %>%
+        mutate(best = bestValues) %>%
+        select(year, population, 
+               best, everything())
+    } else {
+      indicatorEstimates <- indicatorEstimates %>%
+        group_by(year) %>%
+        summarise(min = min(estimate),
+                  max = max(estimate)) %>%
+        ungroup() %>%
+        mutate(best = bestValues) %>%
+        select(year, best, everything())
+    }
+  }
+  
+  # Return the final population data frame
+  return(indicatorEstimates)
+  
+}
+  
+  
+# Plotting functions -----------------------------------------------------
 
 indicatorPlot <- function(data, ylabel = NULL, range = TRUE,
                           xlimits = NULL, facetPlot = NULL, 
@@ -238,10 +386,10 @@ indicatorPlot <- function(data, ylabel = NULL, range = TRUE,
   # Create the plot
   if (range) {
     if (plotGroups) {
-      plot <- ggplot(data = data, aes(x = year, group = groupPlot)) +
-        geom_ribbon(aes(ymin = min, ymax = max),
-                    fill = groupPlot, alpha = 0.4) +
-        geom_line(aes(y = best, colour = groupPlot)) +
+      plot <- ggplot(data = data, aes_string(x = "year", group = groupPlot)) +
+        geom_ribbon(aes_string(ymin = "min", ymax = "max", 
+                               fill = groupPlot), alpha = 0.4) +
+        geom_line(aes_string(y = "best", colour = groupPlot)) +
         xlab("Year") + ylab(ylabel) +
         plotOpts + theme(legend.position = "right")
     } else {  
@@ -254,8 +402,8 @@ indicatorPlot <- function(data, ylabel = NULL, range = TRUE,
     }
   } else {
     if (plotGroups) {
-      plot <- ggplot(data = data, aes(x = year, group = groupPlot)) +
-        geom_line(aes(y = best, colour = groupPlot)) +
+      plot <- ggplot(data = data, aes_string(x = "year", group = groupPlot)) +
+        geom_line(aes_string(y = "best", colour = groupPlot)) +
         xlab("Year") + ylab(ylabel) +
         plotOpts + theme(legend.position = "right")
     } else {  
@@ -267,7 +415,7 @@ indicatorPlot <- function(data, ylabel = NULL, range = TRUE,
   }
   
   plot <- plot + coord_cartesian(xlim = xlimits[1:2]) +
-    scale_x_continuous(breaks = seq(xlimits[1], xlimits[1], 
+    scale_x_continuous(breaks = seq(xlimits[1], xlimits[2], 
                                     by = xlimits[3]))
 
   if (plotFacets) {
